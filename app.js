e95b189262791539192b0b25582f020b565038cd
lovekesh Kumar <bk062428@gmail.com>
2023-03-16 11:08:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  const input = parseInt(req.query.number)
-  let output = ''
-  if(input % 5 === 0){
-    output +='L'
-  }if(input % 7 === 0 ){
-    output +='R'
-  } if (output === ' '){
-    output = input
+app.get("/", (req, res) => {
+  const input = parseInt(req.query.number);
+
+  if (isNaN(input)) {
+    return res
+      .status(400)
+      .json({ error: "Invalid input. Please provide a valid integer number." });
   }
-  res.json({output})
-})  
+  let output = input;
+  if (input % 5 === 0 && input % 7 === 0) {
+    output = "LR";
+  } else if (input % 5 === 0) {
+    output = "L";
+  } else if (input % 7 === 0) {
+    output = "R";
+  }
+  res.json({ output });
+});
+
 app.listen(3000, () => {
-  console.log("server started on port 3000");
+  console.log("Server started on port 3000");
 });
